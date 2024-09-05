cb36d8c3ced0719e9363704f94ec7e92a8c32f7f
Marten. <marten.aun@tkt.ee>
2024-09-05 13:39:09 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is acomment for my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+//string-sõne
+// = salvesta väärtus
+
+Console.WriteLine("Enter your name:"); //output 
+string userName = Console.ReadLine(); //input
+Console.WriteLine("Hello" +","+ userName + "!"); //output
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //input
